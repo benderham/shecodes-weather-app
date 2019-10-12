f5bddae5557fc2e2c5cd2398b4182bcec5df4b59
Ben Derham <bderham@marketforce.com.au>
2019-10-12 18:42:00 +0800

    Save Week 13 code into a sub-folder.

---
+++ Week 13/src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import CurrentWeather from './components/CurrentWeather';
+
+function App() {
+  return <CurrentWeather city="Perth" temperature="34" />;
+}
+
+export default App;
