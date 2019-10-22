141714a78a7778ac91e092bd0754ab2f93f31499
Ben Derham <bderham@marketforce.com.au>
2019-10-22 21:07:13 +0800

    added Week 15 - Props folder

---
+++ Week 15 - Props/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ComplaintPage from './pages/ComplaintPage';
+import WeatherPage from './pages/WeatherPage';
+
+function App() {
+  return (
+    <>
+      <WeatherPage />
+      <ComplaintPage />
+    </>
+  );
+}
+
+export default App;
