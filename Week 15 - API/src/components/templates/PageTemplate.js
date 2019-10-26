928c9d3ec480996523879c76174fad618a487cd2
Ben Derham <bderham@marketforce.com.au>
2019-10-26 12:42:00 +0800

    add api folder

--- Week 15 - API/src/components/templates/PageTemplate.js
+++ Week 15 - API/src/components/templates/PageTemplate.js
@@ -1,22 +1,14 @@
 import React from 'react';
-import { WeatherContext } from '../../containers/AppContainer';
 import CurrentWeather from '../molecules/CurrentWeather';
 
-const PageTemplate = ({ ...props }) => {
+const PageTemplate = ({ city, temperature, changeCity, ...props }) => {
   return (
     <div>
-      <WeatherContext.Consumer>
-        {({ city, temperature, changeCity }) => {
-          console.log(temperature);
-          return (
-            <CurrentWeather
-              city={city}
-              temperature={temperature}
-              onTextLinkClick={changeCity}
-            />
-          );
-        }}
-      </WeatherContext.Consumer>
+      <CurrentWeather
+        city={city}
+        temperature={temperature}
+        onTextLinkClick={changeCity}
+      />
       {props.children}
     </div>
   );
--- Week 15 - API/src/containers/AppContainer.js
+++ Week 15 - API/src/containers/AppContainer.js
@@ -3,16 +3,13 @@ import { Route } from 'react-router-dom';
 import ComplaintPage from '../pages/ComplaintPage';
 import WeatherPage from '../pages/WeatherPage';
 
-export const WeatherContext = React.createContext(); // Create a context for the Weather.
-
-const CITIES = ['London', 'Paris', 'Perth', 'Tokyo', 'Sydney'];
+const CITIES = ['London', 'Perth', 'Tokyo', 'Rome', 'Singapore', 'Sydney'];
 
 // WARNING: Do not store any secrets (such as private API keys) in your React app!
 // https://create-react-app.dev/docs/adding-custom-environment-variables/
-const API_URL = 'http://localhost:8080/api.openweathermap.org/data/2.5';
+const API_URL = 'https://api.openweathermap.org/data/2.5';
 
 class AppContainer extends Component {
-  // note: drilling "Forecast" down to the Forecast component will be a pain, we want to provide it to the whole app using context.
   constructor(props) {
     super(props);
 
@@ -22,30 +19,25 @@ class AppContainer extends Component {
       forecast: []
     };
 
-    this.changeCity = this.changeCity.bind(this);
+    this.handleCityChange = this.handleCityChange.bind(this);
   }
 
-  getRandomCity(array) {
-    return array[Math.floor(Math.random() * array.length)];
-  }
-
-  changeCity() {
-    this.setState({ city: this.getRandomCity(CITIES) }, async () => {
-      await this.fetchCurrentWeather();
-      this.fetchForecast();
-    });
-  }
-
-  fetchCurrentWeather() {
+  fetchApiData() {
     fetch(
       `${API_URL}/weather?q=${this.state.city}&units=metric&appid=${process.env.REACT_APP_API_KEY}`
-    )
-      .then(response => response.json())
+    ) // Call the fetch function passing the url of the API as a parameter
+      .then(response => {
+        console.log(response);
+        return response.json();
+        // Your code for handling the data you get from the API
+      })
       .then(data => {
+        console.log(data);
         this.setState({ temperature: data.main.temp });
       })
-      .catch(err => {
+      .catch(function(err) {
         console.error(err);
+        // This is where you run code if the server returns any errors
       });
   }
 
@@ -62,19 +54,53 @@ class AppContainer extends Component {
       });
   }
 
-  async componentDidMount() {
-    await this.fetchCurrentWeather();
+  getRandomCity(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
+
+  handleCityChange() {
+    this.setState({ city: this.getRandomCity(CITIES) }, () => {
+      this.fetchApiData();
+      this.fetchForecast();
+    });
+  }
+
+  componentDidMount() {
+    this.fetchApiData();
     this.fetchForecast();
   }
 
   render() {
     return (
-      <WeatherContext.Provider // Populate context with our state object!
-        value={{ ...this.state, changeCity: this.changeCity }}
-      >
-        <Route path="/" exact component={WeatherPage} />
-        <Route path="/complain" component={ComplaintPage} />
-      </WeatherContext.Provider>
+      <>
+        <Route
+          path="/"
+          component={() => {
+            return (
+              <WeatherPage
+                city={this.state.city}
+                temperature={this.state.temperature}
+                forecast={this.state.forecast}
+                changeCity={this.handleCityChange}
+              />
+            );
+          }}
+          exact
+        />
+
+        <Route
+          path="/complain"
+          component={() => {
+            return (
+              <ComplaintPage
+                city={this.state.city}
+                temperature={this.state.temperature}
+                changeCity={this.handleCityChange}
+              />
+            );
+          }}
+        />
+      </>
     );
   }
 }
